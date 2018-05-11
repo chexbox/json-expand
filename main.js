2dac4f7e915ccff882e082826c1385716ad82c6c
chexbox <31783961+chexbox@users.noreply.github.com>
2018-05-11 09:26:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
 function expand(text) {
   let a = 0; //counter for which character you're on
-  let output;
+  let newtext = "";
   let par = false;
   let indent = 0;
   while (a < text.length) {
     if (text[a] !== " ") {
-      output = output + text[a];
-      if (text[a] == '"' && text[a - 1] !== "\\") {
-        par = !par;
+      newtext = newtext + text[a];
+    };
+    a += 1;
+  };
+  a = 0;
+  let output = "";
+  while (a < newtext.length) {
+    output = output + newtext[a];
+    if (newtext[a] == '"' && newtext[a - 1] !== "\\") {
+      par = !par;
+    };
+    if (newtext[a] == "{" || newtext[a] == "[") {
+      indent += 2;
+      if (newtext[a+1] == ",") {
+        output = output + newtext[a] + newtext[a+1] + "\n";
+      } else {
+        output = output + newtext[a] + "\n"
       };
-      if (text[a] == "{" || text[a] == "") {
-        indent += 2;
+      let i;
+      for (i = 0; i < indent; i++) {
+        output = output + " ";
       };
+    };
+    if (newtext[a] = "," && newtext[a - 1] !== "}" && newtext[a - 1] !== "]") {
     };
     a += 1;
   };
