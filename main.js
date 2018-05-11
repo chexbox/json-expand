50a72c0a4b96567b0cf05950f7adff72dbf27ac0
chexbox <31783961+chexbox@users.noreply.github.com>
2018-05-10 20:39:23 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+function expand(text) {
+  let a = 0; //counter for which character you're on
+  let output;
+  let par;
+  let last; //stores the last digit added to output
+  while (a < input.length) {
+    
+    a += 1;
+  };
+};
